052d0f6af77d4acd1c3e9e6928f7ff6d6d2aaf91
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-03-08 20:25:53 -0300

    Classe Empregado atualizada. Adioado metodos de calculo de salario

--- Empregado.java
+++ Empregado.java
@@ -1,12 +1,54 @@
 public class Empregado{
     private String nome;
-    private int Idade;
+    private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
 
+    public double calcularSalario(){
+        switch (tipo){
+            case 1:
+                return salario;
+            case 2:
+                return salario + salario * comissao;
+            case 3:
+                return salario + bonus;
+            default:
+                return 0;
+        }
+    }
+    //getters
+    public int getTipo() {
+        return tipo;
+    }
+    public double getBonus() {
+        return bonus;
+    }
+    public double getComissao() {
+        return comissao;
+    }
+    public double getSalario() {
+        return salario;
+    }
     public int getIdade() {
-        return Idade;
+        return idade;
     }
     public String getNome() {
         return nome;
+    }
+    //setters
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
     }
     public void setIdade(int idade) {
         Idade = idade;
@@ -14,4 +56,5 @@ public class Empregado{
     public void setNome(String nome) {
         this.nome = nome;
     }
+    
 }
