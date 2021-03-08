1bd5f6ec79bac6c4dd2865408aa12322ae42e472
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-03-08 20:05:42 -0300

    Classe Emprado Criadaaaaa

---
+++ Empregado.java
@@ -0,0 +1,17 @@
+public class Empregado{
+    private String nome;
+    private int Idade;
+
+    public int getIdade() {
+        return Idade;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setIdade(int idade) {
+        Idade = idade;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
