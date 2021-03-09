33fed45129e9c1b98233d0ec6b47cc00d68f98f0
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-03-08 21:11:03 -0300

    Primeiro teste executado.

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -3,5 +3,19 @@ public class TesteEmpregado {
         Empregado e1 = new Empregado();
         Empregado e2 = new Empregado();
         Empregado e3 = new Empregado();
+
+        e1.setTipo(1);
+        e2.setTipo(2);
+        e3.setTipo(3);
+
+        e1.setSalario(2000);
+        e2.setSalario(1400);
+        e2.setComissao(0.5);
+        e3.setSalario(1500);
+        e3.setBonus(700);
+
+        System.out.println(e1.calcularSalario());
+        System.out.println(e2.calcularSalario());
+        System.out.println(e3.calcularSalario());
     }
 }
